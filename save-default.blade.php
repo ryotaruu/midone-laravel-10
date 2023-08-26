4dfe883ff0355885e91879a4c203122a755d5d67
senkiysma0 <senkiysma@gmail.com>
2023-08-26 10:12:30 +0700

    first

---
+++ save-default.blade.php
@@ -0,0 +1,37 @@
+{{-- package.json --}}
+{{-- {
+    "private": true,
+    "type": "module",
+    "scripts": {
+        "dev": "vite",
+        "build": "vite build"
+    },
+    "devDependencies": {
+        "axios": "^1.1.2",
+        "laravel-vite-plugin": "^0.8.0",
+        "vite": "^4.0.0"
+    }
+} --}}
+<?php
+
+namespace App\Http\Middleware;
+
+use Fideloper\Proxy\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array|string
+     */
+    protected $proxies;
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    // protected $headers = Request::HEADER_X_FORWARDED_ALL;
+}
